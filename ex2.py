1819928e51c619b91912c023c28a2b1a40c64039
Flávia <flavia.sousaneri@gmail.com>
2019-02-20 21:56:14 -0300

    Listar aleatoriamente e diz o maior valor

---
+++ ex2.py
@@ -0,0 +1,15 @@
+import random as r
+lista=[]
+listapar=[]
+listaimpar=[]
+
+for item in range(10):
+    lista.append(r.randrange(1,100))
+    
+def mostrar(lista):
+    print(lista)
+    if item % 2=0:
+        listapar.append = item
+    else:
+        listaimpar[]=item
+    return print(f'IMPAR: {listaimpar}')
